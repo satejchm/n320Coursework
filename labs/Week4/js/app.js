77a16bad7de164f34550a74886e9412232a7b7fe
Sarah Tejchma <satejchm@iu.edu>
2019-09-26 15:54:57 -0400

    re-submit of lab week 4

--- labs/Week4/js/app.js
+++ labs/Week4/js/app.js
@@ -2,8 +2,61 @@
 //Animation Lab
 //9-22-2019
 
-var longRect = document.querySelector("#longRect");
-var cardHolder = document.querySelector("#cardHolder");
+let newHeader = document.createElement("div");
+newHeader.classList.add("header");
+newHeader.innerHTML = "<h1>Animation Lab</h1>";
+headerHolder.appendChild(newHeader);
+
+//set up connections and events for this rectangle div
+newHeader.addEventListener("mouseover", onh1Over);
+newHeader.addEventListener("mouseout", onh1Out);
+newHeader.addEventListener("mousedown", onh1Click);
+
+//this function refers to the css class to add animation for when you hover over rect
+function onh1Over(event) {
+  event.target.style.animationDelay = "0s";
+  event.target.classList.add("h1Over");
+  event.target.classList.remove("h1Out");
+}
+
+//this functionr refers to the css class to add animation for when you move outside the rect
+function onh1Out(event) {
+  event.target.classList.add("h1Out");
+  event.target.classList.remove("h1Over");
+}
+
+//this function refers to css class so that when you click the rect it exits and dissapears off the page.
+function onh1Click(event) {
+  event.target.classList.add("h1Click");
+}
+
+//creating the longer rectangle div across the top of the other divs
+let newRect = document.createElement("div");
+newRect.classList.add("longRect");
+rectHolder.appendChild(newRect);
+
+//set up connections and events for this rectangle div
+newRect.addEventListener("mouseover", onRectOver);
+newRect.addEventListener("mouseout", onRectOut);
+newRect.addEventListener("mousedown", onRectClick);
+
+//this function refers to the css class to add animation for when you hover over rect
+function onRectOver(event) {
+  event.target.style.animationDelay = "0s";
+  event.target.classList.add("rectOver");
+  event.target.classList.remove("rectOut");
+}
+
+//this functionr refers to the css class to add animation for when you move outside the rect
+function onRectOut(event) {
+  event.target.classList.add("rectOut");
+  event.target.classList.remove("rectOver");
+}
+
+//this function refers to css class so that when you click the rect it exits and dissapears off the page.
+function onRectClick(event) {
+  event.target.classList.add("rectClick");
+}
 
 //create a for loop to put 6 of the cards/divs onto the page
 var i = 0;
@@ -13,7 +66,7 @@ for (i = 0; i < 6; i++) {
   newCard.classList.add("card");
   newCard.style.animationDelay = i * 0.1 + "s";
 
-  //set up connections and events
+  //set up connections and events for the card divs below rectangle div
   newCard.addEventListener("mouseover", onCardOver);
   newCard.addEventListener("mouseout", onCardOut);
   newCard.addEventListener("mousedown", onCardClick);
