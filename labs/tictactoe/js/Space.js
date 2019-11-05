113a1946420d65e1d1fc2225261c936d3ac3315d
Sarah Tejchma <satejchm@iu.edu>
2019-11-04 19:58:25 -0500

    updating changes and adding tic tac toe
    for next assignment

---
+++ labs/tictactoe/js/Space.js
@@ -0,0 +1,8 @@
+class Space {
+  constructor() {
+    //when game starts, spaces are empty or null
+    this.value = null;
+    //will signify winner by changing color of the space
+    this.isWinner = false;
+  }
+}
